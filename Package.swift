baa5b92cc0af5d253e3c1784e94008bdd6701e32
Nihad Samedov <nihadsamedov0@gmail.com>
2025-02-21 17:53:26 +0300

    revert

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
 // swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -14,17 +16,17 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/sqlcipher/sqlcipher.git", exact: "4.5.5"),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YapDatabase",
-            dependencies: [
-                .product(name: "sqlcipher", package: "sqlcipher")
-            ],
+            dependencies: [],
             cSettings: [
-                .headerSearchPath("privateInclude"),
-                .define("SQLITE_HAS_CODEC")
+                .headerSearchPath("privateInclude")
             ]
         ),
         .target(
@@ -33,7 +35,6 @@ let package = Package(
         ),
         .testTarget(
             name: "SwiftYapDatabaseTests",
-            dependencies: ["SwiftYapDatabase"]
-        ),
+            dependencies: ["SwiftYapDatabase"]),
     ]
 )
